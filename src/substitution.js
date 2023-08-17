e335f86b0f9adf37f48ec96143538e8bec9b6750
Annie Kevra <anniekevra95@gmail.com>
2023-08-17 12:00:06 -0400

    completed polybius tests and substitution fxn

--- src/substitution.js
+++ src/substitution.js
@@ -3,11 +3,45 @@
 // Only add code (helper methods, variables, etc.) within the scope
 // of the anonymous function on line 6
 
+function uniqueCharacters(alphabetString) {
+  // any two characters the same-return false
+  for (let i = 0; i < alphabetString.length; i++)
+    for (let j = i + 1; j < alphabetString.length; j++)
+      if (alphabetString[i] == alphabetString[j]) return true;
+}
+
 const substitutionModule = (function () {
   // you can add any code you want within this function scope
-
-  function substitution(input, alphabet, encode = true) {
-    // your solution code here
+  let alphaLookup = "abcdefghijklmnopqrstuvwxyz";
+  function substitution(input = "", alphabet = "", encode = true) {
+    //alphabet must be 26 characters long and each character must be unique
+    if (alphabet.length !== 26 || uniqueCharacters(alphabet)) {
+      return false;
+    }
+    result = "";
+    const substituObj = {};
+    //loop through given alphabet string and associate each character with a letter of the alphabet
+    for (let i = 0; i < alphabet.length; i++) {
+      //push to new object
+      if (encode) {
+        //if encode is true (need real letter to be key- to return hidden letter)
+        substituObj[alphaLookup[i]] = alphabet[i];
+      } else {
+        //in encode if false (need fake letter to be key)
+        substituObj[alphabet[i]] = alphaLookup[i];
+      }
+    }
+    inputMessage = input.toLowerCase();
+    //loop through input message and associate each character with real letter
+    for (let i = 0; i < inputMessage.length; i++) {
+      const messageChar = inputMessage[i];
+      if (messageChar === " ") {
+        result += messageChar;
+      } else {
+        result += substituObj[messageChar];
+      }
+    }
+    return result;
   }
 
   return {
--- test/polybius.test.js
+++ test/polybius.test.js
@@ -1,1 +1,72 @@
-// Write your tests here!
+const { expect } = require("chai");
+const { polybius } = require("../src/polybius");
+
+describe("polybius ()", () => {
+  describe("encoding a message", () => {
+    it("should translate 'i' and 'j' to 42", () => {
+      const input = "jiggle";
+      const actual = polybius(input);
+      const expected = "424222221351";
+
+      expect(actual).to.equal(expected);
+    });
+    describe("maintains spaces", () => {
+      it("maintains spaces: in the message", () => {
+        const input = "my message";
+        const actual = polybius(input);
+        const expected = "2345 23513434112251";
+
+        expect(actual).to.equal(expected);
+      });
+      it("maintains spaces: does not add space before or after the message", () => {
+        const input = "my message";
+        const actual = polybius(input);
+        const expected = "2345 23513434112251";
+
+        expect(actual).to.equal(expected);
+      });
+    });
+    describe("ignores capital letters: result of Message and message are the same", () => {
+      it("ignores capital letters: result of Message)", () => {
+        const input = "message";
+        const actual = polybius(input);
+        const expected = "23513434112251";
+
+        expect(actual).to.equal(expected);
+      });
+      it("ignores capital letters: result of message)", () => {
+        const input = "message";
+        const actual = polybius(input);
+        const expected = "23513434112251";
+
+        expect(actual).to.equal(expected);
+      });
+    });
+  });
+
+  describe("decoding a message", () => {
+    it("should translate 42 to both 'i' and 'j'", () => {
+      const input = "424222221351";
+      const actual = polybius(input, false);
+
+      expect(actual).to.include("i");
+      expect(actual).to.include("j");
+    });
+    describe("maintains spaces", () => {
+      it("maintains spaces: in the message", () => {
+        const input = "2345 23513434112251";
+        const actual = polybius(input, false);
+        const expected = "my message";
+
+        expect(actual).to.equal(expected);
+      });
+      it("maintains spaces: does not add space before or after the message", () => {
+        const input = "2345 23513434112251";
+        const actual = polybius(input, false);
+        const expected = "my message";
+
+        expect(actual).to.equal(expected);
+      });
+    });
+  });
+});
